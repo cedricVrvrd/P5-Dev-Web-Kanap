9935a88c45c84fbdd33461f5a711565668ab4b8b
Cedric Vervoord <cedric.immo@live.fr>
2022-08-17 11:40:39 +0200

    lien avec page products

---
+++ front/js/product.js
@@ -0,0 +1,31 @@
+// const urlLocation = window.location.href;
+// console.log(urlLocation)
+
+const str = window.location.href;
+const url = new URL(str);
+let idUrl = url.searchParams.get("id");
+let urlGlobale = "http://localhost:3000/api/products/" + idUrl;
+console.log(idUrl);
+console.log(urlGlobale)
+
+
+fetch(urlGlobale)
+.then(res => {
+     if(res.ok){
+         console.log(res)
+         res.json().then(data =>{
+                let img = document.querySelector(".item__img");
+                img.innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
+                let title = document.querySelector("#title");
+                title.innerHTML = data.name;
+                let price = document.querySelector("#price");
+                price.innerHTML = `${data.price}`;
+                let description = document.querySelector("#description");
+                description.innerHTML = data.description;
+                
+         }) 
+     }
+     else{
+         console.log("erreur de dialogue avec l'API.")
+     }
+})
