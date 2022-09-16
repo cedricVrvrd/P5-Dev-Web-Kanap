f089025fc946b54d94a926a83b7209c67b1259e9
Cedric Vervoord <cedric.immo@live.fr>
2022-09-16 11:22:37 +0200

    COMMENTAIRES

--- front/js/product.js
+++ front/js/product.js
@@ -1,5 +1,8 @@
-// On récupere l'id puis on l'ajoute dans l'url
 
+//--------------------------------------------------
+//  RECUPERATION ID
+//--------------------------------------------------
+// On récupere l'id puis on l'ajoute dans l'url afin de reupére le bon produit
 const str = window.location.href;
 const url = new URL(str);
 let idUrl = url.searchParams.get("id");
@@ -7,7 +10,10 @@ let urlGlobale = "http://localhost:3000/api/products/" + idUrl;
 console.log(idUrl);
 console.log(urlGlobale)
 
-// 
+//--------------------------------------------------
+// RECUPERATION PRODUIT SUIVANT ID
+//--------------------------------------------------
+// on requete Api pour récuprer les élements du produit puis les ajouter 
 fetch(urlGlobale)
     .then(res => {
         if (res.ok) {
@@ -30,8 +36,13 @@ fetch(urlGlobale)
     .catch(error => {console.log(error);
     });
 
-const button = document.querySelector("#addToCart")
 
+//--------------------------------------------------
+// AJOUT DANS LE PANIER
+//--------------------------------------------------
+// on recupère l'element bouton
+const button = document.querySelector("#addToCart")
+// puis on ajoute le produit dans le panier 
 button.addEventListener("click", function () {
     const qty = parseInt(document.querySelector("#quantity").value);
     const couleur = document.querySelector("#colors").value;
@@ -41,11 +52,19 @@ button.addEventListener("click", function () {
         couleur: couleur,
     }
     addBasket(product)
-    console.log("le type est " + typeof qty)
+    // contôle type et quantité
+    // console.log("le type est " + typeof qty) 
 
 });
+
+
 //--------------------------------------------------
-// on recupére le panier, si il est vide on retourne un tableau, on push le produit dans tableau, on sauve.
+// on recupére le panier, si il est vide on retourne un tableau, 
+// si quantité != 0 et couleur  alors on push le produit
+// on sauvegarde la panier
+//--------------------------------------------------
+//Autrement------------------------------------------------
+// si produit dans le panier, on tri le panier, si produit identiques = ajuste la quantité, si pas de produit identique, on push
 //--------------------------------------------------
 function addBasket(product) {
     let basket = JSON.parse(localStorage.getItem('basket'));
@@ -56,12 +75,9 @@ function addBasket(product) {
         }
         localStorage.setItem('basket', JSON.stringify(basket))
     }
-    //--------------------------------------------------
-    // si produit dans le panier, on tri le panier, si produit identiques = ajuste la quantirté, si pas de produit identique, on push
-    //--------------------------------------------------
     else {
         let _product = basket.find(p => p.id === product.id && p.couleur === product.couleur);
-        // console.log("62", _product);
+        // console.log("70", _product);
         if(_product){
             _product.qty += product.qty
         }
