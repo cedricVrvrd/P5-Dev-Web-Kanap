bda9eba6256ed1df5d89579fa36fc20849e9498c
Cedric Vervoord <cedric.immo@live.fr>
2022-09-16 15:05:37 +0200

    correction et clean / page terminée

--- front/js/script.js
+++ front/js/script.js
@@ -1,36 +1,50 @@
 // récuperartion de l'adresse API
-    const url = "http://localhost:3000/api/products";
+const url = "http://localhost:3000/api/products";
+
+
 
 //--------------------------------------------------
 // Requête API pour demander l'ensemble des produits, on recupère la reponse et on parcours les
-//  données (data)
+//  données (data) et on les ajoutes au DOM
 //--------------------------------------------------
-   fetch(url)
-   .then(res => {
-        if(res.ok){
+// si erreur de requete affiche et renvoie l'erreur
+fetch(url)
+    .then(res => {
+        if (res.ok) {
             console.log(res)
-            res.json().then(data =>{
-                let productsSection = document.querySelector("#items");
+            res.json().then(data => {
                 for (let index = 0; index < data.length; index++) {
-                   let productsSectionItems = `
-                    <a href="./product.html?id=${data[index]._id}">
-     <article>
-      <img src="${data[index].imageUrl}" alt="${data[index].altTxt}"/>
-      <h3 class="productName">${data[index].name}</h3>
-     <p class="productDescription">${data[index].description}</p>
-    </article>
-   </a>
-                    `   
-                    productsSection.innerHTML += productsSectionItems; 
+                    // lien
+                    let link = document.createElement("a");
+                    document.querySelector("#items").appendChild(link);
+                    link.href = `product.html?id=${data[index]._id}`;
+                    // section article
+                    let sectionArticle = document.createElement("article");
+                    link.appendChild(sectionArticle);
+                    // image et alt
+                    let productImg = document.createElement("img");
+                    sectionArticle.appendChild(productImg);
+                    productImg.src = data[index].imageUrl;
+                    productImg.alt = data[index].altTxt;
+                    //  nom du produit
+                    let productName = document.createElement("h3");
+                    sectionArticle.appendChild(productName);
+                    productName.className = "productName";
+                    productName.textContent = data[index].name;
+                    //  description du prodruit
+                    let productDescription = document.createElement("p");
+                    sectionArticle.appendChild(productDescription);
+                    productDescription.className = "productDescription";
+                    productDescription.textContent = data[index].description;
                 }
             })
         }
-        else{
+        else {
             console.log("erreur de dialogue avec l'API.")
         }
-   })
-   .catch(error => {
-    console.log("33 erreur lors de l'envoi", error);
-})
+    })
+    .catch(error => {
+        console.log("erreur lors de l'envoi", error);
+    })
 
 
